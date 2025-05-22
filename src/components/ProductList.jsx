dcc295a64f9ccfaab1b635966c6a81beb67d0065
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-05-22 13:07:37 +0900

    feat: display products

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -14,18 +14,18 @@ import ProductCard from './ProductCard';
  * @type {Array<Product>}
  */
 const products = [
-    { id: 1, name: '무선 블루투스 이어폰', price: 59000, img: '/sample.png' },
-    { id: 2, name: '게이밍 키보드', price: 79000, img: '/sample.png' },
-    { id: 3, name: '스마트 워치', price: 129000, img: '/sample.png' },
-    { id: 4, name: '핸디 청소기', price: 89000, img: '/sample.png' },
-    { id: 5, name: 'LED 스탠드', price: 35000, img: '/sample.png' },
-    { id: 6, name: '무선 마우스', price: 29000, img: '/sample.png' },
-    { id: 7, name: '아이폰 충전 케이블', price: 12000, img: '/sample.png' },
-    { id: 8, name: 'USB 허브 4포트', price: 22000, img: '/sample.png' },
-    { id: 9, name: '커피 원두 1kg', price: 45000, img: '/sample.png' },
-    { id: 10, name: '무드등 가습기', price: 69000, img: '/sample.png' },
-    { id: 11, name: '라면 5개입', price: 5500, img: '/sample.png' },
-    { id: 12, name: '고양이 사료 2kg', price: 29000, img: '/sample.png' },
+    { id: 1, name: '무선 블루투스 이어폰', price: 59000, img: '/01.png' },
+    { id: 2, name: '게이밍 키보드', price: 79000, img: '/02.png' },
+    { id: 3, name: '스마트 워치', price: 129000, img: '/03.png' },
+    { id: 4, name: '핸디 청소기', price: 89000, img: '/04.png' },
+    { id: 5, name: 'LED 스탠드', price: 35000, img: '/05.png' },
+    { id: 6, name: '무선 마우스', price: 29000, img: '/06.png' },
+    { id: 7, name: '아이폰 충전 케이블', price: 12000, img: '/07.png' },
+    { id: 8, name: 'USB 허브 4포트', price: 22000, img: '/08.png' },
+    { id: 9, name: '커피 원두 1kg', price: 45000, img: '/09.png' },
+    { id: 10, name: '무드등 가습기', price: 69000, img: '/10.png' },
+    { id: 11, name: '라면 5개입', price: 5500, img: '/11.png' },
+    { id: 12, name: '고양이 사료 2kg', price: 29000, img: '/12.png' },
 ];
 
 //TODO: ProductList 컴포넌트를 완성하세요.
@@ -38,7 +38,13 @@ const products = [
 
 /** @param {ProductListProps} props */
 const ProductList = ({ addToCart }) => {
-    return <div className="product-list"></div>;
+    return (
+        <div className="product-list">
+            {products.map((product) => (
+                <ProductCard product={product} addToCart={addToCart} key={product.id} />
+            ))}
+        </div>
+    );
 };
 
 export default ProductList;
